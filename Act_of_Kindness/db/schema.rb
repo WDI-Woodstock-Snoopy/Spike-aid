6e2c3ce41d4c1cee5672c5b1f52aa514655ad867
Tyler Geneva <ctg55@yahoo.com>
2015-05-11 15:28:39 -0500

    adds requires in application.js

--- Act_of_Kindness/db/schema.rb
+++ Act_of_Kindness/db/schema.rb
@@ -47,10 +47,6 @@ ActiveRecord::Schema.define(version: 20150511174311) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
-    t.integer  "posts_count"
-    t.integer  "upvotes_count"
-    t.integer  "score"
-    t.string   "location"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "lat"
