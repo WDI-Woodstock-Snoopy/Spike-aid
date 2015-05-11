a5df048d7256adaa590704e1da127708b61ef53d
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-11 12:43:38 -0500

    posts migration includes lat/long columns

--- Act_of_Kindness/db/schema.rb
+++ Act_of_Kindness/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511174058) do
+ActiveRecord::Schema.define(version: 20150511174311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150511174058) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "long"
+    t.string   "lat"
   end
 
   create_table "stems", force: :cascade do |t|
