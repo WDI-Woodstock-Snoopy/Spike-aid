c053f9583d0eea802d1b4c0f2ea538822962a2cc
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-11 12:42:23 -0500

    users migration includes lat/long columns

--- Act_of_Kindness/db/schema.rb
+++ Act_of_Kindness/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511151415) do
+ActiveRecord::Schema.define(version: 20150511174058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,14 @@ ActiveRecord::Schema.define(version: 20150511151415) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
+    t.integer  "posts_count"
+    t.integer  "upvotes_count"
+    t.integer  "score"
+    t.string   "location"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "lat"
+    t.string   "long"
   end
 
   add_foreign_key "helpers", "users"
